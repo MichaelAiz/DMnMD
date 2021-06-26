4f33e751ee28c399e2445983396a23c5a7a79f50
shrinjay <shrinjaymukherjee@gmail.com>
2021-06-26 16:25:17 -0600

    Initi route

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/receiveMsg', methods=['GET', 'POST'])
+def recieve_msg():
+    message = request.values.get('Body', None)
+    phone = request.values.get('From')
+    print(message)
 
 
+app.run()
