64b559544c431e36869ff13f02104fe7bf3b0fc9
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2021-06-26 19:51:08 -0400

    edited text

--- gc_demo.py
+++ gc_demo.py
@@ -4,7 +4,7 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "E:/Projects/EngHack/auth.json"
 client = language_v1.LanguageServiceClient()
 
-text = "Hello doctor. I have 3 ants a broken wife an old desk. Also there is no way I am not awesome and old whore standing blood clots rotten apples."
+text = "Hello doctor. Recently I feel my back hurting. It hurts when I sit and when I walk."
 document = {"content": text, "type_": language_v1.Document.Type.PLAIN_TEXT}
 response = client.classify_text(request={'document': document})
 
