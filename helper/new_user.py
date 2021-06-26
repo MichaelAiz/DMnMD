f2530da64de5971e50e98ce1bb28d7c79aeadab7
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2021-06-26 15:18:48 -0400

    added twilio message

---
+++ helper/new_user.py
@@ -0,0 +1,22 @@
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+#from create_new_user import
+
+load_dotenv()
+
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+def new_user():
+    message = client.messages \
+                .create(
+                     body="Please enter your name and phone number",
+                     from_='+1',
+                     to='+1'
+                 )
+    
+
+
