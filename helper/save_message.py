0eaa38af76d7b0ecf7ce993e8e434be031ed65da
Lucas Wiecek <35616733+LYcheck@users.noreply.github.com>
2021-06-27 00:20:15 -0400

    tested push

---
+++ helper/save_message.py
@@ -0,0 +1,6 @@
+from data.db_functions import get_user, create_new_message
+
+def save_message(phone_number, message):
+    user_id = get_user(phone=phone_number)[0]['_id']
+
+    create_new_message(user_id, message)
