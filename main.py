e46031b0d8e5d3254be2ff90139d3cd232902a69
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2021-06-26 23:18:06 -0400

    added under_twenty to functions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from helper.function_selector import function_selector
 from helper.twilio_helper import generate_response
 from helper.new_user import new_user
 from helper.over_twenty import over_twenty
+from helper.under_twenty import under_twenty
 from helper.confirm_registration import confirm_registration
 import os
 
@@ -14,7 +15,8 @@ os.environ["PYTHONPATH"] = os.getcwd()
 FUNCTIONS = {
     'new_user': new_user,
     "confirm_registration": confirm_registration,
-    "over_twenty": over_twenty
+    "over_twenty": over_twenty,
+    "under_twenty": under_twenty
 }
 
 
